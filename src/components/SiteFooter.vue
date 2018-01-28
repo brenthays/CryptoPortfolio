186299cb956b402715cdccffd14d1bf5b7f753fc
Brent <brenthays711@gmail.com>
2018-01-28 04:02:28 -0600

    adds donate

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="footer">
+    <b-modal
+    id="donate-modal"
+    title="Donate"
+    ref="modalDonate">
+      <p>Bitcoin Address: 1LNpgPX36WGpBQGnStQyfXLstdHVPvyd2K</p>
+      <p>Ethereum: 0x0cf9541156Be3029224BC058e77D24F4B52349fE</p>
+      <p>Litecoin: Li2HWmbxzBE8EhfLALsbSnKVaejYvDyzdY</p>
+      <div slot="modal-footer" class="w-100">
+        <b-btn class="float-right" variant="secondary" v-on:click="closeModal">
+          Close
+        </b-btn>
+      </div>
+    </b-modal>
     <div class="container">
       <div class="row footer-row">
         <div class="col-12 text-center">
@@ -9,6 +22,14 @@
             data-ad-client="ca-pub-1317079355092968"
             data-ad-slot="6004053934"
             data-ad-format="auto"></ins>
+        </div>
+      </div>
+      <div class="row footer-row">
+        <div class="col-12">
+          <hr/>
+        </div>
+        <div class="col-12">
+          <a href="#" v-b-modal.donate-modal>Donate</a>
         </div>
       </div>
     </div>
@@ -30,6 +51,9 @@
     methods: {
       signOut: function () {
         firebase.auth().signOut()
+      },
+      closeModal: function () {
+        this.$refs.modalDonate.hide()
       }
     },
 
