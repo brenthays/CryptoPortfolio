e1c26741bd3237275e2bc2deef2b29cb0b1d0f9f
Brent <brenthays711@gmail.com>
2018-06-09 20:23:18 -0500

    updates landing page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="row main-row">
     <div class="col-12">
-      <div class="auth-box text-center">
+      <div class="jumbotron text-center">
         <div>
           <h2>Track Your Cryptocurrency in Real Time</h2>
-          <!-- <div class="row highlight">
+          <div class="row highlight">
             <div class="col-xs-12 col-md-2">
 
             </div>
             <div class="col-xs-12 col-md-4 item">
               <img src="../assets/images/coins.svg"/>
-              <h3>Track All Your Coins in One Place</h3>
+              <h5>Track All Your Coins in<br/>One Place</h5>
             </div>
             <div class="col-xs-12 col-md-4 item">
               <img src="../assets/images/realtime.svg"/>
-              <h3>See Your Portfolio Value in Real Time</h3>
+              <h5>View Your Portfolio Value<br/>in Real Time</h5>
             </div>
-            <div class="col-xs-12 col-md-4">
+            <div class="col-xs-12 col-md-2">
 
             </div>
-          </div> -->
-          <a href="/login" title="Sign Up for Free" class="btn btn-primary" v-show="!loading && !authUser">Sign Up For Free</a>
-          <a href="/portfolio" title="Go To Your Portfolio" v-show="!loading && authUser" class="btn btn-primary">Go To Your Portfolio</a>
+          </div>
+          <a href="/login" title="Sign Up for Free" class="btn btn-primary" v-show="!loading && !authUser">Sign Up For Free <i class="fa fa-caret-right"></i></a>
+          <a href="/portfolio" title="Go To Your Portfolio" v-show="!loading && authUser" class="btn btn-primary">Go To Your Portfolio <i class="fa fa-caret-right"></i></a>
         </div>
       </div>
     </div>
